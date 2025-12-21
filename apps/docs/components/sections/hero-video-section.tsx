ee869e7c1a88b32aa29cf200b86efb5016fdb5ed
Ritesh Buchas <bucharitesh@gmail.com>
2025-12-22 04:56:00 +0530

    chore: update ui

--- apps/docs/components/sections/hero-video-section.tsx
+++ apps/docs/components/sections/hero-video-section.tsx
@@ -1,8 +1,15 @@
+import { HeroVideoDialog } from "@/components/ui/hero-video-dialog";
+
 export function HeroVideoSection() {
   return (
     <div className="relative px-6 mt-10">
-      <div className="flex items-center justify-center relative size-full shadow-xl rounded-2xl overflow-hidden aspect-video bg-muted border border-border">
-        Dummy Placeholder
+      <div className="relative size-full shadow-xl rounded-2xl overflow-hidden">
+        <HeroVideoDialog
+          animationStyle="from-center"
+          videoSrc="https://www.youtube.com/embed/z7dsFh9Vcco?si=7dvjiGuRPHWMJdCm"
+          thumbnailSrc="https://img.youtube.com/vi/z7dsFh9Vcco/maxresdefault.jpg"
+          thumbnailAlt="QRdx Introduction Video"
+        />
       </div>
     </div>
   );
